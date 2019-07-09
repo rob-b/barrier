be90f2f761aa4c91046785893aad4280dda54338
Rob Berry <rob.berry@zerodeposit.com>
2019-07-09 13:48:38 +0100

    Add comment with link to story

--- src/Barrier/Events/PullRequest.hs
+++ src/Barrier/Events/PullRequest.hs
@@ -8,33 +8,31 @@
 module Barrier.Events.PullRequest where
 
 import           Barrier.Check                (extractClubhouseLinks, extractClubhouseLinks2)
-import           Barrier.Clubhouse            (ClubhouseLink (ClubhouseLink), Story, StoryError,
-                                               getStory, mkClubhouseStoryUrl, unClubhouseLink)
+import           Barrier.Clubhouse            (ClubhouseLink, Story, StoryError (StoryHttpError),
+                                               getStory, mkClubhouseStoryUrl)
 import           Barrier.Config               (AppConfig, readish)
 import           Barrier.Events.Types         (WrappedHook (WrappedHookPullRequest),
                                                unWrapHookPullRequest)
 import           Barrier.GitHub               (addStoryLinkComment, getCommentsForPullRequest,
                                                setHasStoryStatus, setMissingStoryStatus)
-import           Barrier.ListUtils            (ordNub)
 import           Control.Error                (ExceptT, runExceptT)
-import           Control.Logger.Simple        (logDebug, logError)
-import           Control.Monad                (when)
+import           Control.Logger.Simple        (logDebug)
+import           Control.Monad                (unless)
 import           Control.Monad.IO.Class       (MonadIO)
 import           Control.Monad.Reader         (runReaderT)
 import           Data.Aeson                   (Value, object, (.=))
-import           Data.Either                  (lefts, partitionEithers)
+import           Data.Either                  (lefts)
 import           Data.Maybe                   (listToMaybe, maybeToList)
 import           Data.Monoid                  ((<>))
 import           Data.Text                    (Text)
 import qualified Data.Text                    as T
 import qualified Data.Vector                  as V
-import           GitHub.Data.Issues           (issueCommentBody)
+import           GitHub.Data.Issues           (IssueComment, issueCommentBody)
 import           GitHub.Data.Webhooks.Events  (PullRequestEvent, PullRequestEventAction (PullRequestActionOther, PullRequestEditedAction, PullRequestOpenedAction, PullRequestReopenedAction),
                                                evPullReqAction, evPullReqPayload)
 import           GitHub.Data.Webhooks.Payload (HookPullRequest, getUrl, whPullReqHead,
                                                whPullReqHtmlUrl, whPullReqTargetRef)
 import           Text.Regex.PCRE.Heavy        (re, scan)
-import           URI.ByteString               (Absolute, URIRef)
 
 
 --------------------------------------------------------------------------------
@@ -73,12 +71,6 @@ handlePullRequestAction pr = do
   pure $ setPullRequestStatus (storyFromRef <> links) (unWrapHookPullRequest payload)
 
 
-xo :: Maybe ClubhouseLink -> [ClubhouseLink] -> AppConfig -> IO ()
-xo Nothing [] _config  = putStrLn "Check the comments, set the status, maybe add a comment"
-xo Nothing xs _config  = putStrLn "Check the comments, "
-xo (Just x) xs _config = undefined
-
-
 --------------------------------------------------------------------------------
 getStoryLinkFromHook :: WrappedHook -> Maybe ClubhouseLink
 getStoryLinkFromHook hook = do
@@ -101,7 +93,7 @@ setPullRequestStatus links payload config = do
       logDebug $ "No story could be detected from " <> getUrl (whPullReqHtmlUrl payload)
       mapM_ (logDebug . T.pack . show) errors
       pure []
-    Right stories -> pure $ ordNub stories
+    Right stories -> pure stories
   checkerAndUpdater config payload stories
 
 
@@ -124,65 +116,24 @@ extractStoryId value = extract ((listToMaybe . scan regex) value) >>= readish
 
 
 --------------------------------------------------------------------------------
-checkUpdatePullRequest
-  :: AppConfig
-  -> HookPullRequest
-  -> URIRef Absolute
-  -> [URIRef Absolute]
-  -> IO ()
-checkUpdatePullRequest config payload linkFromRefE linksFromPRDescE = do
-  refStoryLink <- getStoryLink linkFromRefE
-
-  storyLinksE <- mapM getStoryLink linksFromPRDescE
-  (errors, stories) <- fmap partitionEithers (traverse runExceptT storyLinksE)
-
-  issueCommentsE <- getCommentsForPullRequest config payload
-
-  runExceptT refStoryLink >>= \case
-    Right story  -> do
-      setHasStoryStatus config payload story
-      -- FIXME before adding the link we need to check that the comments don't already include a
-      -- link. The PR data only includes the PR itself and so we will miss comments that include
-      -- the link
-      case issueCommentsE of
-        Left err       -> logError $ T.pack ("Error retrieving comments: " <> show err)
-        Right comments -> do
-
-          let bodies = concatMap extractClubhouseLinks2 (fmap issueCommentBody comments)
-          bodiesE <- mapM getStoryLink (fmap unClubhouseLink bodies)
-          (bodyErrors, bodyStories) <- fmap partitionEithers (traverse runExceptT bodiesE)
-
-          mapM_ (logDebug . T.pack . show) bodyErrors
-
-          -- no stories in either the description or the comments? Add a comment with the link
-          when (null (stories <> bodyStories)) $ addStoryLinkComment config payload story
-
-    -- we could not extract a story link from the commit ref, lets try getting a link from the PR
-    -- description instead
-    Left err -> do
-      -- log the error encountered while extracting a link from the ref
-      (logDebug . T.pack . show) err
-
-      -- log any errors encountered while extract links from the description
-      mapM_ (logDebug . T.pack . show) errors
-
-      -- set an appropriate status for our list of stories
-      _ <- checkerAndUpdater config payload stories
-      pure ()
-  where
-    getStoryLink link  = linkDebug link >> runReaderT (getStory (ClubhouseLink link)) config
-
-
---------------------------------------------------------------------------------
 checkerAndUpdater :: AppConfig
                   -> HookPullRequest
-                  -> [Story]
+                  -> [FoundStory]
                   -> IO ()
 checkerAndUpdater config payload stories = do
   setStatus stories
   where
-    setStatus (story:_) = setHasStoryStatus config payload story
-    setStatus []        = setMissingStoryStatus config payload
+    commentsOnly fs =
+      case foundSource fs of
+        CommentSource -> True
+        _             -> False
+
+    setStatus [] = setMissingStoryStatus config payload
+    setStatus stories'@(story:_) = do
+      let story' = foundStory story
+      setHasStoryStatus config payload story'
+      unless (any commentsOnly stories') (addStoryLinkComment config payload story')
+      pure ()
 
 
 --------------------------------------------------------------------------------
@@ -193,27 +144,43 @@ sequenceEithers xs =
       Left _       -> Left (lefts xs)
 
 
+data StorySource
+  = RefSource
+  | DescriptionSource
+  | CommentSource
+  deriving (Show)
+
+
+data FoundStory = FoundStory
+  { foundSource :: StorySource
+  , foundStory  :: Story
+  }
+
 --------------------------------------------------------------------------------
 getStoriesOrDieTrying ::
-     AppConfig -> HookPullRequest -> [ClubhouseLink] -> IO (Either [StoryError] [Story])
+     AppConfig -> HookPullRequest -> [ClubhouseLink] -> IO (Either [StoryError] [FoundStory])
 getStoriesOrDieTrying config payload links = do
   -- stories gathered from the PR refname and the PR description
   sts <- mapM (getStoryForLink config) links
   stories <- traverse runExceptT sts
 
-  -- comments on the PR
   issueCommentsE <- getCommentsForPullRequest config payload
-  case issueCommentsE of
-    Left err -> do
-      when (null stories) $ addStoryLinkComment config payload _
-      logDebug $ T.pack (show err)
-      pure $ sequenceEithers stories
-    Right comments -> do
+  issueComments <- case issueCommentsE of
+    Left err    -> pure [Left (StoryHttpError (show err))]
+    Right comms -> storiesFromComments comms
+
+  pure $ sequenceEithers (fmap convertToDescription stories <> fmap convertToComment issueComments)
+
+  where
+    storiesFromComments :: V.Vector IssueComment -> IO [Either StoryError Story]
+    storiesFromComments comments = do
       let bodies = concatMap extractClubhouseLinks2 (fmap issueCommentBody comments)
       bodiesE <- mapM (getStoryForLink config) bodies
-      (errors', bodies') <- partitionEithers <$> traverse runExceptT bodiesE
-      logDebug . T.pack $ show errors'
-      pure . sequenceEithers $ stories <> fmap Right bodies'
+      traverse runExceptT bodiesE
+
+
+    convertToDescription = fmap (FoundStory DescriptionSource)
+    convertToComment = fmap (FoundStory CommentSource)
 
 
 linkDebug :: (Show a, MonadIO m) => a -> m ()
